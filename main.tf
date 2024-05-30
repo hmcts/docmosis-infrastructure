85ab98055e2726c0263829550c4b516b334a5728
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-05-30 12:54:29 +0100

    Import rg to resolve pipeline error

--- main.tf
+++ main.tf
@@ -8,3 +8,8 @@ resource "azurerm_resource_group" "infrastructure_resource_group" {
 
   tags = local.tags
 }
+
+import {
+  to = azurerm_resource_group.infrastructure_resource_group
+  id = "/subscriptions/****/resourceGroups/docmosis-infrastructure-aat"
+}
