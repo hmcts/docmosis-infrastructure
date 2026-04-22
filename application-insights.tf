0698f0f9c2854bc508fc829d62f585998afb6521
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-04-22 15:50:58 +0100

    use 4.x branch

--- application-insights.tf
+++ application-insights.tf
@@ -1,10 +1,9 @@
 module "application_insights" {
-  source = "git@github.com:hmcts/terraform-module-application-insights?ref=main"
+  source = "git@github.com:hmcts/terraform-module-application-insights?ref=4.x"
 
   env      = var.env
   product  = var.product
   name     = var.product
-  location = var.location
 
   resource_group_name = azurerm_resource_group.infrastructure_resource_group.name
 
