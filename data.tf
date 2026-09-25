c9ee435cbd74dee1bc9045451d3786950de8bdb8
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-09-25 09:18:28 +0100

    use sbox for kv

--- data.tf
+++ data.tf
@@ -1,4 +1,4 @@
 data "azurerm_user_assigned_identity" "jenkins" {
-  name                = "jenkins-${var.env}-mi"
+  name                = var.env == "sandbox" ? "jenkins-sbox-mi" : "jenkins-${var.env}-mi"
   resource_group_name = "managed-identities-${var.env}-rg"
 }
